82aed5ca180c10373533a194bd8746d7f4d81e74
OpanDilaga <51019063+OpanDilaga@users.noreply.github.com>
2019-05-29 13:42:16 +0700

    Java
    Upload Java

---
+++ PBOJ&S-A2.1700084-Latihan60/src/latihan60/akatsuki.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan60;
+
+/**
+ *
+ * @author acer
+ * Nama     : Opan Dilaga
+ * NIM      : A2.1700084
+ * Kelas    : TI IV E J&S
+ */
+public class akatsuki {
+    String namatokoh;
+
+public akatsuki(){
+    
+}
+public String getnamatokoh(){
+    return namatokoh;
+}
+public void setnamatokoh(String namatokoh){
+    this.namatokoh = namatokoh;
+}
+
+}
